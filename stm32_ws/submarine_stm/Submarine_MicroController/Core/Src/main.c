fc5d0bfb18b364b7c8fe84e4f8cc09d7101fcbaa
Dyllon <duntondyllon@gmail.com>
2023-08-24 01:25:10 -0400

    Enable global interupt for lpuart on sub

--- stm32_ws/submarine_stm/Submarine_MicroController/Core/Src/main.c
+++ stm32_ws/submarine_stm/Submarine_MicroController/Core/Src/main.c
@@ -18,7 +18,6 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "string.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
