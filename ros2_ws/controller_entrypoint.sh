2deb53c82d0ddf04b110492d3a8a04fb9c63e431
jacobcwildes <wildes126@gmail.com>
2023-08-25 17:12:14 +0000

    Added a base docker image - needs debug

---
+++ ros2_ws/controller_entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source /opt/ros/foxy/setup.bash
+
+colcon build
+
+source install/setup.bash
+
+cd launch
+
+xpra start --bind-tcp=0.0.0.0:10000 --mdns=no --ssl=off --webcam=no --no-daemon --start "ros2 launch con_launch.py"
+
