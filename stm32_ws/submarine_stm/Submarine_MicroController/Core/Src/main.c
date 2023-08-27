f402ebabe37694f7cfd86c2fa7b9e3206de1c24d
Dyllon <duntondyllon@gmail.com>
2023-08-26 20:43:12 -0400

    Set up servo timers

--- stm32_ws/submarine_stm/Submarine_MicroController/Core/Src/main.c
+++ stm32_ws/submarine_stm/Submarine_MicroController/Core/Src/main.c
@@ -660,9 +660,9 @@ static void MX_TIM5_Init(void)
   htim5.Instance = TIM5;
   htim5.Init.Prescaler = 0;
   htim5.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim5.Init.Period = 4294967295;
+  htim5.Init.Period = 2399999;
   htim5.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-  htim5.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  htim5.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
   if (HAL_TIM_Base_Init(&htim5) != HAL_OK)
   {
     Error_Handler();
