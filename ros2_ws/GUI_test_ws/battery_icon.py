baff07e271a97631efc8979903700914c16229bc
jacobcwildes <wildes126@gmail.com>
2023-09-15 06:54:27 +0000

    Made ballast icon testbench work - GUI mostly(?) operational

--- ros2_ws/GUI_test_ws/battery_icon.py
+++ ros2_ws/GUI_test_ws/battery_icon.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2 as cv
 import time
+from datetime import datetime
 
 
 ##DIMS - THESE CHANGE SO I DONT HAVE TO CONSTANTLY UPDATE OTHER NUMBERS
@@ -14,6 +15,22 @@ compass_bottom_y = 10
 compass_top_x = 700
 compass_top_y = 30
 
+ballast_cap_l = np.array([[10, 200], [15, 190], [20, 185], [25, 185], [30, 190],  [35, 200]], np.int32)
+ballast_cap_l = ballast_cap_l.reshape((-1, 1, 2))
+ballast_stern_l = np.array([[10, 300], [15, 310], [30, 310], [35, 300]], np.int32)
+ballast_stern_l = ballast_stern_l.reshape((-1, 1, 2))
+hull_cap = np.array([[55, 175], [60, 170], [65, 165], [70, 160], [75, 155], [110, 155], [115, 160], [120, 165], [125, 170], [130, 175]], np.int32)
+hull_cap = hull_cap.reshape((-1, 1, 2))
+hull_stern = np.array([[55, 330], [60, 340], [125, 340], [130, 330]], np.int32)
+hull_stern = hull_stern.reshape((-1, 1, 2))
+ballast_cap_r = np.array([[150, 200], [155, 190], [160, 185], [165, 185], [170, 190], [175, 200]], np.int32)
+ballast_cap_r = ballast_cap_r.reshape((-1, 1, 2))
+ballast_stern_r = np.array([[150, 300], [155, 310], [160, 310], [170, 310], [175, 300]], np.int32)
+ballast_stern_r = ballast_stern_r.reshape((-1, 1, 2))
+
+#Start the program running counter:
+prog_start = datetime.now()
+
 #Create a blank image
 img = np.zeros((512, 512, 3), np.uint8)
 
@@ -26,6 +43,51 @@ cv.rectangle(img, (battery_bottom_x, battery_bottom_y), (battery_top_x, battery_
 cv.rectangle(img, (battery_bottom_x, battery_bottom_y), (battery_top_x, battery_top_y), (255, 255, 255), 3)
 #Draw terminal nib
 cv.rectangle(img, (battery_top_x, 25), (battery_top_x + 10, 35), (255, 255, 255), -1)
+    
+#Draw left ballast base
+cv.fillPoly(img, [ballast_cap_l], (150, 150, 150))
+cv.rectangle(img, (10, 200), (35, 300), (150, 150, 150), -1)
+cv.fillPoly(img, [ballast_stern_l], (150, 150, 150))
+
+#Draw the left ballast tank outline
+cv.polylines(img, [ballast_cap_l], True, (255, 255, 255), 2, cv.LINE_AA)
+cv.rectangle(img, (10, 200), (35, 300), (255, 255, 255), 2)
+cv.polylines(img, [ballast_stern_l], True, (255, 255, 255), 2, cv.LINE_AA)
+
+#Draw right ballast base
+cv.fillPoly(img, [ballast_cap_r], (150, 150, 150))
+cv.rectangle(img, (150, 200), (175, 300), (150, 150, 150), -1)
+cv.fillPoly(img, [ballast_stern_r], (150, 150, 150))
+
+#Draw the right ballast tank outline
+cv.polylines(img, [ballast_cap_r], True, (255, 255, 255), 2, cv.LINE_AA)
+cv.rectangle(img, (150, 200), (175, 300), (255, 255, 255), 2)
+cv.polylines(img, [ballast_stern_r], True, (255, 255, 255), 2, cv.LINE_AA)
+
+#Draw connector base
+cv.rectangle(img, (35, 220), (55, 230), (150, 150, 150), -1)
+cv.rectangle(img, (150, 220), (130, 230), (150, 150, 150), -1)
+cv.rectangle(img, (35, 245), (55, 255), (150, 150, 150), -1)
+cv.rectangle(img, (150, 245), (130, 255), (150, 150, 150), -1)
+cv.rectangle(img, (35, 270), (55, 280), (150, 150, 150), -1)
+cv.rectangle(img, (150, 270), (130, 280), (150, 150, 150), -1)
+#Draw the connector outlines
+cv.rectangle(img, (35, 220), (55, 230), (255, 255, 255), 2)
+cv.rectangle(img, (150, 220), (130, 230), (255, 255, 255), 2)
+cv.rectangle(img, (35, 245), (55, 255), (255, 255, 255), 2)
+cv.rectangle(img, (150, 245), (130, 255), (255, 255, 255), 2)
+cv.rectangle(img, (35, 270), (55, 280), (255, 255, 255), 2)
+cv.rectangle(img, (150, 270), (130, 280), (255, 255, 255), 2)
+ 
+#Draw the hull base
+cv.fillPoly(img, [hull_cap], (150, 150, 150))
+cv.rectangle(img, (55, 175), (130, 330), (150, 150, 150), -1)
+cv.fillPoly(img, [hull_stern], (150, 150, 150))
+
+#Draw the hull outline
+cv.polylines(img, [hull_cap], True, (255, 255, 255), 2, cv.LINE_AA)
+cv.rectangle(img, (55, 175), (130, 330), (255, 255, 255), 2)
+cv.polylines(img, [hull_stern], True, (255, 255, 255), 2, cv.LINE_AA)
 
 #Draw message console
 cv.rectangle(img, (0, 560), (900, 600), (255, 255, 255), -1)
@@ -33,7 +95,6 @@ cv.rectangle(img, (0, 560), (900, 600), (255, 255, 255), -1)
 cv.rectangle(img, (920, 560), (1024, 600), (255, 255, 255), -1)
 #Label
 cv.putText(img, "Console log", (400, 590), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 1, cv.LINE_AA)
-cv.putText(img, "00h:00m:00s", (930, 590), cv.FONT_HERSHEY_SIMPLEX, .45, (0, 0, 0), 1, cv.LINE_AA)
 cv.imshow("Window", img)
 
 #Compass base
@@ -57,7 +118,7 @@ cv.line(img, p1, p3, (255, 255, 255), 2)
 #The length of the compass box - 100 (the size of the two sidebars) is 300/90 (The desired amount of degrees shown)
 #After a lot of subjective testing, the above does work, but it doesn't look particularly great. This number makes 
 #The headings appear much more nicely than before
-scale_deg = 2.15
+scale_deg = 2.18
 
 directions = ["N", "NW", "W", "SW", "S", "SE", "E", "NE"]
 
@@ -125,7 +186,7 @@ while True:
                 if degree > 180: #Cut 360 in half so we go from -180- -> 180 (put NW, W, and SW on the left)
                     degree -= 360
                 if(degree >= -60 and degree <= 60):
-                    cv.putText(img, str(directions[cardinal]), (int(degree*scale_deg + 500), 25), cv.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255), 1, cv.LINE_AA)
+                    cv.putText(img, str(directions[cardinal]), (int(degree*scale_deg + 495), 25), cv.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255), 1, cv.LINE_AA)
            
         #Write the actual heading in the compass right box
         #Will clean this up so that it's a struct or something that I stringify instead. Maybe. Either way there are going to be a bunch of "if" statements checking for the heading
@@ -161,6 +222,29 @@ while True:
         cv.rectangle(img, (0,0), (22, 22), (0, 0, 0), -1)
         cv.putText(img, str(int(FPS)), (2, 15), cv.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255), 1, cv.LINE_AA)
         #This is not a perfect indication of the FPS in this case, but it is the logic that needs to be used in the execution (i.e. it won't be in a bunch of for loops because there won't BE for loops) Also, this will be directly impacted by how long the waitkey is. The waitkey in actual program will not wait at all
-        cv.imshow("Window", img)
-        cv.waitKey(40)
         
+        prog_current = datetime.now()
+        prog_time = prog_current - prog_start
+        
+        #Remove microseconds from time delta
+        prog_time = str(prog_time).split(".")[0]
+        #Draw uptime counter - won't be a thing in regular program, just needed for testbench
+        cv.rectangle(img, (920, 560), (1024, 600), (255, 255, 255), -1)
+        cv.putText(img, str(prog_time), (930, 585), cv.FONT_HERSHEY_SIMPLEX, .65, (0, 0, 0), 1, cv.LINE_AA)
+        cv.imshow("Window", img)
+        cv.waitKey(20)
+    
+    #Simulate the ballasts filling
+    for i in range(98):
+        #Since our ballasts fill from front to back, show fill from top->bottom
+        
+        #Left ballast
+        cv.rectangle(img, (12, 201), (33, 201), (150, 150, 150), -1)
+        cv.rectangle(img, (12, 201), (33, 201 + i), (255, 0, 0), -1)
+        
+        #Right ballast
+        cv.rectangle(img, (152, 201), (173, 201), (150, 150, 150), -1)
+        cv.rectangle(img, (152, 201), (173, 201 + i), (255, 0, 0), -1)
+        
+        cv.imshow("Window", img)
+        cv.waitKey(20)
