44f03c5a28810afe3a0daa4f768ac4fe4ef75d7e
Dyllon <duntondyllon@gmail.com>
2023-11-21 13:44:49 -0500

    Switch PWM to PD15 rather than PD13

--- stm32_ws/submarine_stm/Submarine_MicroController/Core/Src/stm32l4xx_hal_msp.c
+++ stm32_ws/submarine_stm/Submarine_MicroController/Core/Src/stm32l4xx_hal_msp.c
@@ -460,10 +460,10 @@ void HAL_TIM_MspPostInit(TIM_HandleTypeDef* htim)
 
     __HAL_RCC_GPIOD_CLK_ENABLE();
     /**TIM4 GPIO Configuration
-    PD13     ------> TIM4_CH2
     PD14     ------> TIM4_CH3
+    PD15     ------> TIM4_CH4
     */
-    GPIO_InitStruct.Pin = GPIO_PIN_13|GPIO_PIN_14;
+    GPIO_InitStruct.Pin = GPIO_PIN_14|GPIO_PIN_15;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
