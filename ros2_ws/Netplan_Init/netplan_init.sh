c66c8d8d980202b32ed4320326f6800121318b14
jacobcwildes <wildes126@gmail.com>
2023-11-24 05:15:28 +0000

    Need to bypass some security

--- ros2_ws/Netplan_Init/netplan_init.sh
+++ ros2_ws/Netplan_Init/netplan_init.sh
@@ -8,8 +8,8 @@ ip_address_pi2="192.168.1.70"
 
 #Function to apply and generate netplan
 apply_netplan(){
-    /usr/sbin/netplan generate
-    /usr/sbin/netplan apply
+    sudo /usr/sbin/netplan generate >> /tmp/netplan_generate.log 2>&1 
+    sudo /usr/sbin/netplan apply >> /tmp/netplan_apply.log 2>&1 
 }
 
 #Loop until Pis can ping each other
@@ -17,10 +17,10 @@ apply_netplan(){
 while : 
 do
     if ping -c 1 -W 1 "$ip_address_pi1" && ping -c 1 -W 1 "$ip_address_pi2"; then
-        echo "Both Pis can ping!"
+        echo "Both Pis can ping!" >> /tmp/ping_status.log
         break
     else
-        echo "Pis unable to ping, retrying in 5s"
+        echo "Pis unable to ping, retrying in 5s" >> /tmp/ping_status.log
         sleep 5 #Wait 5 seconds
         #Generate and apply netplan config
          apply_netplan
