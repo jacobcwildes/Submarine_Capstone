d73ee4ba6d8c504860cc347da84e63b42af31f89
jacobcwildes <wildes126@gmail.com>
2023-12-11 23:35:02 -0500

    Bring Python commenting up to standard for PyLint

--- ros2_ws/src/controller_gui/controller_gui/con_gui.py
+++ ros2_ws/src/controller_gui/controller_gui/con_gui.py
@@ -1,3 +1,9 @@
+'''
+Primary controller GUI module which takes camera frames and passes
+them off to the overlay function to be modified. Those images
+are then displayed
+'''
+
 #Import ROS2 libraries
 import rclpy
 from rclpy.node import Node
@@ -7,7 +13,7 @@ from message_filters import Subscriber, TimeSynchronizer
 from rclpy import qos
 
 #Import custom message type
-#Note that DataInfo needed a header for ROS2 to add a timestamp to. Without it, the 
+#Note that DataInfo needed a header for ROS2 to add a timestamp to. Without it, the
 #time synchro fails because it can never retrieve a time
 from com_interfaces.msg import DataInfo
 from com_interfaces.msg import ComInfo
@@ -36,167 +42,189 @@ from overlay import overlay
 import RPi.GPIO as gpio
 
 class GUI(Node):
-
+    '''
+    This class deals with making data, camera, and controller message
+    subscribers. The running time is also calculated within the methods
+    of this class. FPS is calculated and the controller GUI is visualized
+    here
+    '''
     def __init__(self):
         #Track the start of the program
         self.prog_start = datetime.now()
         super().__init__('controller_gui')
-        
-        #Make an object that holds subscription info for the cam (UDP) 
-        self.cam_sub = self.create_subscription(Image, 'camera/image', self.cam_callback, qos.qos_profile_sensor_data) #10
+
+        #Make an object that holds subscription info for the cam (UDP)
+        self.cam_sub = self.create_subscription(Image, 'camera/image',
+                                                self.cam_callback, qos.qos_profile_sensor_data)
 
         #Data subscription will go here (UDP)
-        self.data_sub = self.create_subscription(DataInfo, 'data_info', self.data_callback, qos.qos_profile_sensor_data) #10
-        
+        self.data_sub = self.create_subscription(DataInfo, 'data_info',
+                                                 self.data_callback, qos.qos_profile_sensor_data)
+
         #Command subscription goes here (for screenshots) UDP
-        self.screenshot_sub = self.create_subscription(ComInfo, 'com_info', self.screenshot_callback, qos.qos_profile_sensor_data) #10
-        
+        self.screenshot_sub = self.create_subscription(ComInfo, 'com_info',
+                                                       self.screenshot_callback,
+                                                       qos.qos_profile_sensor_data)
+
         #Previous time (for FPS calc)
         self.previous_time = 0
-        
+
         #Make the object that will convert a ROS2 image message to
         #an OpenCV image format
-        self.Bridge = CvBridge()
-        
+        self.bridge = CvBridge()
+
         #Tkinter (GUI)
         self.root = tk.Tk()
         self.root.title("Submarine View")
-        
-        '''
-        #Force the environment variable to override if necessary
-        if os.printenviron.get('DISPLAY', '') == '':
-            os.environ.__setitem__('DISPLAY', ':0.0')
-        '''
-        
+
         #Make window fullscreen
         self.root.attributes('-fullscreen', True)
-        
+
         #Have window fit the screen
         #self.root.geometry("%dx%d" % (self.screen_width, self.screen_height))
-         
-        self.panelA = None
-        
+
+        self.panel_a = None
+
         #Variables to hold data information
-        self.batteryVoltage = 12
-        self.ballastLeft = 0
-        self.ballastRight = 0
-        self.errMess = " "
+        self.battery_voltage = 12
+        self.ballast_left = 0
+        self.ballast_right = 0
+        self.err_mess = " "
         self.heading = 0
         self.depth = 0
         self.speed = 0
-        
+
         #Screenshot?
         self.screenshot = 0
-        
+
         #Set up FPS measuring pin
         gpio.setmode(gpio.BCM)
         gpio.setup(24, gpio.OUT)
-        
+
     #This will eventually be time synchronized with incoming sub metrics
     def cam_callback(self, cam_sub): #Data will be passed here too
-        
+        '''
+        This method is responsible for capturing the images which flow in from 
+        the camera subscription and passing them off to be modified by the overlay
+        method
+        '''
         #Toggle pin on
         gpio.output(24, 1)
-        
-        #Using monotonic time because I don't really care about real timezones. The 
+
+        #Using monotonic time because I don't really care about real timezones. The
         #monotonic clock naively ticks up - perfect for what I want
         current_time = time.monotonic()
-        
+
         #Convert from ROS2 message to OpenCV image format
-        convert_image = self.Bridge.imgmsg_to_cv2(cam_sub)
-        
+        convert_image = self.bridge.imgmsg_to_cv2(cam_sub)
+
         #OpenCV stores colors in BGR. PIL uses RGB - need to convert
-        RGB_img = cv.cvtColor(convert_image, cv.COLOR_BGR2RGB)
-        
+        rgb_img = cv.cvtColor(convert_image, cv.COLOR_BGR2RGB)
+
         #Although not exactly current at the time of display, close enough for our purposes
         prog_current = datetime.now()
         prog_time = prog_current - self.prog_start
 
         #Overlay data onto the image
-        RGB_img = overlay(RGB_img, self.speed, self.batteryVoltage, 255-self.ballastLeft,
-                          255-self.ballastRight, self.depth, self.heading, prog_time)
-        
-        #Make frame per second count. Isn't perfect, but it is a degree of inaccuracy I am 
+        rgb_img = overlay(rgb_img, self.speed, self.battery_voltage, 255-self.ballast_left,
+                          255-self.ballast_right, self.depth, self.heading, prog_time)
+
+        #Make frame per second count. Isn't perfect, but it is a degree of inaccuracy I am
         #willing to absorb since it is not in our favor
         difference = current_time - self.previous_time
-        FPS = 1/difference
-        
+        fps = 1/difference
+
         self.previous_time = current_time
-        cv.putText(RGB_img, str(int(FPS)), (2, 15), cv.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255), 1, cv.LINE_AA)
-        
-        
+        cv.putText(rgb_img, str(int(fps)), (2, 15), cv.FONT_HERSHEY_SIMPLEX, .5,
+                  (255, 255, 255), 1, cv.LINE_AA)
+
+
         #Current Program time
         prog_time = datetime.now() - self.prog_start
-        
+
         #Remove microseconds from time delta
-        prog_time = str(prog_time).split(".")[0]
-        
+        prog_time = str(prog_time).split('.', maxsplit = 1)[0]
+
         #Draw uptime counter
-        cv.rectangle(RGB_img, (920, 560), (1024, 600), (255, 255, 255), -1)
-        cv.rectangle(RGB_img, (920, 560), (1024, 600), (0, 0, 0), 2)
-        cv.putText(RGB_img, str(prog_time), (930, 585), cv.FONT_HERSHEY_SIMPLEX, .65, (0, 0, 0), 1, cv.LINE_AA)
-        
+        cv.rectangle(rgb_img, (920, 560), (1024, 600), (255, 255, 255), -1)
+        cv.rectangle(rgb_img, (920, 560), (1024, 600), (0, 0, 0), 2)
+        cv.putText(rgb_img, str(prog_time), (930, 585), cv.FONT_HERSHEY_SIMPLEX,
+                   .65, (0, 0, 0), 1, cv.LINE_AA)
+
         #Draw Error Messages
-        cv.rectangle(RGB_img, (0, 560), (900, 600), (255, 255, 255), -1)
-        cv.rectangle(RGB_img, (0, 560), (900, 600), (0, 0, 0), 2)
-        cv.putText(RGB_img, str(self.errMess), (25, 590), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 1, cv.LINE_AA)
-        
+        cv.rectangle(rgb_img, (0, 560), (900, 600), (255, 255, 255), -1)
+        cv.rectangle(rgb_img, (0, 560), (900, 600), (0, 0, 0), 2)
+        cv.putText(rgb_img, str(self.err_mess), (25, 590), cv.FONT_HERSHEY_SIMPLEX,
+                   1, (0, 0, 0), 1, cv.LINE_AA)
+
         #Convert images to PIL format
-        PIL_img = PIL.Image.fromarray(RGB_img)
-                
+        pil_img = PIL.Image.fromarray(rgb_img)
+
         #Rescale image to fit the screen
-        PIL_img = PIL_img.resize((720, 578))
-        
+        pil_img = pil_img.resize((720, 578))
+
         #Convert from PIL to ImageTk format (what actually gets displayed)
-        tk_img = PIL.ImageTk.PhotoImage(PIL_img)
-        
-        
+        tk_img = PIL.ImageTk.PhotoImage(pil_img)
+
         #Save image?
         if self.screenshot:
             path = "/mnt/usb/images/" +  str(prog_time).replace(":", "-") + ".jpg"
-            PIL_img.save(path, 'JPEG')
+            pil_img.save(path, 'JPEG')
 
-        
+
         #Work to display will be shown here
         #Init panels at start
-        if self.panelA is None:
-            self.panelA = tk.Label(image=tk_img)
-            self.panelA.image = tk_img
-            #Tell Tkinter to fill both x and y axes. Additionally, if the 
+        if self.panel_a is None:
+            self.panel_a = tk.Label(image=tk_img)
+            self.panel_a.image = tk_img
+            #Tell Tkinter to fill both x and y axes. Additionally, if the
             #window should change size, the frame will too
-        
+
         #If already intitialized, update
         else:
-            self.panelA.configure(image=tk_img)
-            self.panelA.image = tk_img
-            self.panelA.pack(fill=tk.BOTH, expand = True)
-        
+            self.panel_a.configure(image=tk_img)
+            self.panel_a.image = tk_img
+            self.panel_a.pack(fill=tk.BOTH, expand = True)
+
         self.root.update()
-        
+
         #Toggle pin off
         gpio.output(24, 0)
-     
+
     def data_callback(self, data):
+        '''
+        Take message data in from the appropriate subscriber
+        and set it to class variables so that the values
+        may be accessed for the GUI overlay
+        '''
         self.heading = data.degrees_north
-        self.batteryVoltage = data.voltage_battery
+        self.battery_voltage = data.voltage_battery
         self.depth = data.upward
-        self.errMess = data.error
-        self.ballastLeft = data.ballast_left
-        self.ballastRight = data.ballast_right
+        self.err_mess = data.error
+        self.ballast_left = data.ballast_left
+        self.ballast_right = data.ballast_right
         self.speed = data.forward
-        
+
     def screenshot_callback(self, com):
+        '''
+        This is the controller subscriber. The only
+        value that we care about is the screenshot as this
+        is the only command the controller takes action on
+        '''
         self.screenshot = com.screenshot
-    
-        
-  
+
+
+
 def main(args=None):
+    '''
+    This method actually spins up the ROS node so that it may function
+    '''
     rclpy.init(args=args)
     gui_obj = GUI()
     rclpy.spin(gui_obj)
     rclpy.shutdown()
     #Shut down pin grab
     gpio.cleanup()
-    
+
 if __name__ == '__main__':
     main()
--- ros2_ws/src/controller_gui/controller_gui/overlay.py
+++ ros2_ws/src/controller_gui/controller_gui/overlay.py
@@ -1,3 +1,8 @@
+'''
+This module contains the logic for all GUI drawing. 
+The drawn on image is returned
+Written by Jacob Wildes
+'''
 import cv2 as cv
 import numpy as np
 
@@ -6,7 +11,10 @@ import numpy as np
 #to be as accurate as possible, so I will append that just before the image is passed to Tkinter
 
 def overlay(img, speed, battery, ballast_left, ballast_right, depth, deg, time_start):
-    
+    '''
+    Method that actually does the work described above
+    '''
+
     #Resize the image
     img = cv.resize(img, (1024, 600))
     #Static locations for where each component of the battery/compass is. Image won't be
@@ -20,51 +28,66 @@ def overlay(img, speed, battery, ballast_left, ballast_right, depth, deg, time_s
     compass_bottom_y = 10
     compass_top_x = 700
     compass_top_y = 30
-    
+
     #Since I'm using a polyline to make a custom ballast icon, I make these rather disgusting
     #looking arrays. basically, the numpy arrays hold x and y coordinates that the polyline *must*
     #pass through.
-    ballast_cap_l = np.array([[10, 200], [15, 190], [20, 185], [25, 185], [30, 190],  [35, 200]], np.int32)
+    ballast_cap_l = np.array([[10, 200], [15, 190], [20, 185], [25, 185],
+                              [30, 190],  [35, 200]], np.int32)
     ballast_cap_l = ballast_cap_l.reshape((-1, 1, 2))
     ballast_stern_l = np.array([[10, 300], [15, 310], [30, 310], [35, 300]], np.int32)
     ballast_stern_l = ballast_stern_l.reshape((-1, 1, 2))
-    hull_cap = np.array([[55, 175], [60, 170], [65, 165], [70, 160], [75, 155], [110, 155], [115, 160], [120, 165], [125, 170], [130, 175]], np.int32)
+    hull_cap = np.array([[55, 175], [60, 170], [65, 165], [70, 160], [75, 155],
+                         [110, 155], [115, 160], [120, 165], [125, 170], [130, 175]], np.int32)
     hull_cap = hull_cap.reshape((-1, 1, 2))
     hull_stern = np.array([[55, 330], [60, 340], [125, 340], [130, 330]], np.int32)
     hull_stern = hull_stern.reshape((-1, 1, 2))
-    ballast_cap_r = np.array([[150, 200], [155, 190], [160, 185], [165, 185], [170, 190], [175, 200]], np.int32)
+    ballast_cap_r = np.array([[150, 200], [155, 190], [160, 185], [165, 185],
+                              [170, 190], [175, 200]], np.int32)
     ballast_cap_r = ballast_cap_r.reshape((-1, 1, 2))
-    ballast_stern_r = np.array([[150, 300], [155, 310], [160, 310], [170, 310], [175, 300]], np.int32)
+    ballast_stern_r = np.array([[150, 300], [155, 310], [160, 310], [170, 310],
+                                [175, 300]], np.int32)
     ballast_stern_r = ballast_stern_r.reshape((-1, 1, 2))
-    
+
     ##DRAW BATTERY:
     #Draw rectangle backdrop
-    cv.rectangle(img, (battery_bottom_x, battery_bottom_y), (battery_top_x, battery_top_y), (0, 255, 0), -1)
+    cv.rectangle(img, (battery_bottom_x, battery_bottom_y),
+                 (battery_top_x, battery_top_y), (0, 255, 0), -1)
     #Draw rectangle for battery
-    cv.rectangle(img, (battery_bottom_x, battery_bottom_y), (battery_top_x, battery_top_y), (255, 255, 255), 3)
+    cv.rectangle(img, (battery_bottom_x, battery_bottom_y),
+                 (battery_top_x, battery_top_y), (255, 255, 255), 3)
     #Draw terminal nib
     cv.rectangle(img, (battery_top_x, 25), (battery_top_x + 10, 35), (255, 255, 255), -1)
-    
-    #Next, need to correlate the 2.8V range from full to empty to 0->100% (Battery is fully charged at 17V, dead at 12V)
-    #100/2.8 = 35.7. Rectangle is 255 pixels long, so 255/2.8 = 91.07 for color gradient
+
+    #Next, need to correlate the 2.8V range from full to empty to 0->100%
+    #(Battery is fully charged at 17V, dead at 12V)
+    #100/2.8 = 35.7. Rectangle is 255 pixels long, so 255/2.8 = 91.07
+    #for color gradient.
     #Draw red/green bar
-    cv.rectangle(img, (battery_bottom_x, battery_bottom_y), (battery_top_x, battery_top_y),  (0 + int((battery * 20) - 240), (255 - int((battery * 20) - 240)), 0), -1)
-    
+    cv.rectangle(img, (battery_bottom_x, battery_bottom_y),
+                (battery_top_x, battery_top_y),  (0 + int((battery * 20) - 240),
+                (255 - int((battery * 20) - 240)), 0), -1)
+
     #Grey bar
-    cv.rectangle(img, (battery_bottom_x + int((((battery * 20) - 240))), 10), (battery_top_x, 50), (150, 150, 150), -1)
-        
-    cv.putText(img, str(int((battery * 20) - 240)).zfill(2), (930, 40), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1, cv.LINE_AA)
-    
+    cv.rectangle(img, (battery_bottom_x + int((((battery * 20) - 240))), 10),
+                (battery_top_x, 50), (150, 150, 150), -1)
+
+    cv.putText(img, str(int((battery * 20) - 240)).zfill(2), (930, 40),
+               cv.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1, cv.LINE_AA)
+
     ##DRAW COMPASS:
     #Compass base
-    cv.rectangle(img, (compass_bottom_x, compass_bottom_y), (compass_top_x, compass_top_y), (150, 150, 150), -1)
-    cv.rectangle(img, (compass_bottom_x, compass_bottom_y), (compass_top_x, compass_top_y), (255, 255, 255), 2)
+    cv.rectangle(img, (compass_bottom_x, compass_bottom_y),
+                (compass_top_x, compass_top_y), (150, 150, 150), -1)
+    cv.rectangle(img, (compass_bottom_x, compass_bottom_y),
+                (compass_top_x, compass_top_y), (255, 255, 255), 2)
     #Draw left box
-    cv.rectangle(img, (compass_bottom_x, compass_bottom_y), (compass_bottom_x + 50, compass_top_y), (255, 255, 255), 2)
+    cv.rectangle(img, (compass_bottom_x, compass_bottom_y),
+                (compass_bottom_x + 50, compass_top_y), (255, 255, 255), 2)
 
     #Draw right box
-    cv.rectangle(img, (compass_top_x - 50, compass_bottom_y), (compass_top_x, compass_top_y), (255, 255, 255), 2)
-     
+    cv.rectangle(img, (compass_top_x - 50, compass_bottom_y),
+                (compass_top_x, compass_top_y), (255, 255, 255), 2)
 
     #Draw heading pointer
     p1 = (500, 34)
@@ -74,58 +97,76 @@ def overlay(img, speed, battery, ballast_left, ballast_right, depth, deg, time_s
     cv.line(img, p2, p3, (255, 255, 255), 2)
     cv.line(img, p1, p3, (255, 255, 255), 2)
 
-    #The length of the compass box - 100 (the size of the two sidebars) is 300/90 (The desired amount of degrees shown)
-    #After a lot of subjective testing, the above does work, but it doesn't look particularly great. This number makes 
+    #The length of the compass box - 100 (the size of the two sidebars)
+    #is 300/90 (The desired amount of degrees shown)
+    #After a lot of subjective testing, the above does work, but it doesn't
+    #look particularly great. This number makes
     #The headings appear much more nicely than before
     scale_deg = 2.18
 
     directions = ["N", "NW", "W", "SW", "S", "SE", "E", "NE"]
-    #Want to display each heading in the box... Need to make it so that everything gets centered on the point that the arrow is on
+    #Want to display each heading in the box... Need to make it so that
+    #everything gets centered on the point that the arrow is on
     #That way when we're heading North, North is atop the needle... etc. That point is 500
     #Compass base
-    cv.rectangle(img, (compass_bottom_x, compass_bottom_y), (compass_top_x, compass_top_y), (150, 150, 150), -1)
-    cv.rectangle(img, (compass_bottom_x, compass_bottom_y), (compass_top_x, compass_top_y), (255, 255, 255), 2)
-   
+    cv.rectangle(img, (compass_bottom_x, compass_bottom_y),
+                (compass_top_x, compass_top_y), (150, 150, 150), -1)
+    cv.rectangle(img, (compass_bottom_x, compass_bottom_y),
+                (compass_top_x, compass_top_y), (255, 255, 255), 2)
+
     #Draw left box
-    cv.rectangle(img, (compass_bottom_x, compass_bottom_y), (compass_bottom_x + 50, compass_top_y), (255, 255, 255), 2)
+    cv.rectangle(img, (compass_bottom_x, compass_bottom_y),
+                (compass_bottom_x + 50, compass_top_y), (255, 255, 255), 2)
 
     #Draw right box
-    cv.rectangle(img, (compass_top_x - 50, compass_bottom_y), (compass_top_x, compass_top_y), (255, 255, 255), 2)
+    cv.rectangle(img, (compass_top_x - 50, compass_bottom_y),
+                (compass_top_x, compass_top_y), (255, 255, 255), 2)
     #Draw compass headings
     for cardinal in range(8): #each direction label
         degree = deg + cardinal*45 #calculate actual degree in clockwise manner
         if degree > 180: #Cut 360 in half so we go from -180- -> 180 (put NW, W, and SW on the left)
             degree -= 360
         if(degree >= -60 and degree <= 60):
-            cv.putText(img, str(directions[cardinal]), (int(degree*scale_deg + 495), 25), cv.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255), 1, cv.LINE_AA)
+            cv.putText(img, str(directions[cardinal]), (int(degree*scale_deg + 495), 25),
+                       cv.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255), 1, cv.LINE_AA)
     #Write the actual heading in the compass right box
-   
+
     #Check for the heading
     if(0 < deg < 44) or (deg == 360):
-        cv.putText(img, "N", (663, 25), cv.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255), 1, cv.LINE_AA)
-    if(45 < deg < 89):
-        cv.putText(img, "NE", (663, 25), cv.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255), 1, cv.LINE_AA)
-    if(90 < deg < 134):
-        cv.putText(img, "E", (663, 25), cv.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255), 1, cv.LINE_AA)
-    if(135 < deg < 179):
-        cv.putText(img, "SE", (663, 25), cv.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255), 1, cv.LINE_AA)
-    if(180 < deg < 224):
-        cv.putText(img, "S", (663, 25), cv.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255), 1, cv.LINE_AA)
-    if(225 < deg < 269):
-        cv.putText(img, "SW", (663, 25), cv.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255), 1, cv.LINE_AA)
-    if(270 < deg < 314):
-        cv.putText(img, "W", (663, 25), cv.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255), 1, cv.LINE_AA)
-    if(315 < deg < 359):
-        cv.putText(img, "NW", (663, 25), cv.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255), 1, cv.LINE_AA)
+        cv.putText(img, "N", (663, 25), cv.FONT_HERSHEY_SIMPLEX, .5,
+                  (255, 255, 255), 1, cv.LINE_AA)
+    if 45 < deg < 89:
+        cv.putText(img, "NE", (663, 25), cv.FONT_HERSHEY_SIMPLEX, .5,
+                  (255, 255, 255), 1, cv.LINE_AA)
+    if 90 < deg < 134:
+        cv.putText(img, "E", (663, 25), cv.FONT_HERSHEY_SIMPLEX, .5,
+                  (255, 255, 255), 1, cv.LINE_AA)
+    if 135 < deg < 179:
+        cv.putText(img, "SE", (663, 25), cv.FONT_HERSHEY_SIMPLEX, .5,
+                  (255, 255, 255), 1, cv.LINE_AA)
+    if 180 < deg < 224:
+        cv.putText(img, "S", (663, 25), cv.FONT_HERSHEY_SIMPLEX, .5,
+                  (255, 255, 255), 1, cv.LINE_AA)
+    if 225 < deg < 269:
+        cv.putText(img, "SW", (663, 25), cv.FONT_HERSHEY_SIMPLEX, .5,
+                  (255, 255, 255), 1, cv.LINE_AA)
+    if 270 < deg < 314:
+        cv.putText(img, "W", (663, 25), cv.FONT_HERSHEY_SIMPLEX, .5,
+                  (255, 255, 255), 1, cv.LINE_AA)
+    if 315 < deg < 359:
+        cv.putText(img, "NW", (663, 25), cv.FONT_HERSHEY_SIMPLEX, .5,
+                  (255, 255, 255), 1, cv.LINE_AA)
 
     #Draw right box
-    cv.rectangle(img, (compass_top_x - 50, compass_bottom_y), (compass_top_x, compass_top_y), (255, 255, 255), 2)
+    cv.rectangle(img, (compass_top_x - 50, compass_bottom_y),
+                (compass_top_x, compass_top_y), (255, 255, 255), 2)
 
     #Display degree heading
-    cv.putText(img, str(deg), (compass_bottom_x + 10, compass_bottom_y + 15), cv.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255), 1, cv.LINE_AA)
-    cv.rectangle(img, (compass_top_x - 50, compass_bottom_y), (compass_top_x, compass_top_y), (255, 255, 255), 2)
-    
-    
+    cv.putText(img, str(deg), (compass_bottom_x + 10, compass_bottom_y + 15),
+              cv.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255), 1, cv.LINE_AA)
+    cv.rectangle(img, (compass_top_x - 50, compass_bottom_y),
+                (compass_top_x, compass_top_y), (255, 255, 255), 2)
+
     ##DRAW BALLASTS:
     #Draw left ballast base
     cv.fillPoly(img, [ballast_cap_l], (150, 150, 150)) #grey
@@ -161,7 +202,7 @@ def overlay(img, speed, battery, ballast_left, ballast_right, depth, deg, time_s
     cv.rectangle(img, (150, 245), (130, 255), (255, 255, 255), 2)
     cv.rectangle(img, (35, 270), (55, 280), (255, 255, 255), 2)
     cv.rectangle(img, (150, 270), (130, 280), (255, 255, 255), 2)
-     
+
     #Draw the hull base
     cv.fillPoly(img, [hull_cap], (150, 150, 150))
     cv.rectangle(img, (55, 175), (130, 330), (150, 150, 150), -1)
@@ -171,20 +212,21 @@ def overlay(img, speed, battery, ballast_left, ballast_right, depth, deg, time_s
     cv.polylines(img, [hull_cap], True, (255, 255, 255), 2, cv.LINE_AA)
     cv.rectangle(img, (55, 175), (130, 330), (255, 255, 255), 2)
     cv.polylines(img, [hull_stern], True, (255, 255, 255), 2, cv.LINE_AA)
-    
+
     #Since our ballasts fill from front to back, show fill from top->bottom
-        
+
     #Left ballast (the left and right are mirrored so switch them here
     cv.rectangle(img, (12, 201), (33, 201 + int(ballast_right / 2.55)), (0, 0, 255), -1)
     cv.rectangle(img, (12, 201), (33, 201), (150, 150, 150), -1)
-    
+
     #Right ballast
     cv.rectangle(img, (152, 201), (173, 201 + int(ballast_left / 2.55)), (0, 0, 255), -1)
     cv.rectangle(img, (152, 201), (173, 201), (150, 150, 150), -1)
-    
-    
+
     ##DRAW DEPTH:
-    cv.putText(img, "Z Acceleration: ", (10, 540), cv.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255), 1, cv.LINE_AA)
-    cv.putText(img, str(f"{depth:.3f}"), (135, 540), cv.FONT_HERSHEY_SIMPLEX, .5, (255, 255, 255), 1, cv.LINE_AA)
+    cv.putText(img, "Z Acceleration: ", (10, 540), cv.FONT_HERSHEY_SIMPLEX, .5,
+              (255, 255, 255), 1, cv.LINE_AA)
+    cv.putText(img, str(f"{depth:.3f}"), (135, 540), cv.FONT_HERSHEY_SIMPLEX, .5,
+              (255, 255, 255), 1, cv.LINE_AA)
 
     return img
