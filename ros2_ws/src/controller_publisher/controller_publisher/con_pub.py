30bcfd5b3886cb1689d8a58d1f25129f7eec73e5
jacobcwildes <wildes126@gmail.com>
2023-12-11 23:48:52 -0500

    Update Controller Publisher Python file to proper PyLint standard

--- ros2_ws/src/controller_publisher/controller_publisher/con_pub.py
+++ ros2_ws/src/controller_publisher/controller_publisher/con_pub.py
@@ -1,3 +1,8 @@
+'''
+This file deals with making a controller data publishing node.
+Once the node is created this file will continue to run so that the 
+data can be published down to the sub
+'''
 #ROS2 Necessities
 import rclpy
 from rclpy.node import Node
@@ -9,27 +14,34 @@ from com_interfaces.msg import ComInfo
 import serial
 
 class ControllerOutput(Node):
-    
+    '''
+    This class makes the controller publishing node, which publishes data at 
+    20Hz.
+    '''
     def __init__(self):
         #Command publication
         super().__init__('controller_publisher')
         #Make a publisher - first arg is custom defined message, second
         #is topic name, third is how many things to queue to publish (TCP)
         self.com_pub = self.create_publisher(ComInfo, 'com_info', qos.qos_profile_services_default)
-        
+
         #Serial read setup
-        self.serialport = None
-        self.serialLine = None
-        
-        #Publish data 
+        self.serial_port = None
+        self.serial_line = None
+
+        #Publish data
         timer_period = 0.05
         self.timer = self.create_timer(timer_period, self.timer_callback)
-        
-        
+
     def timer_callback(self):
+        '''
+        This callback is responsible for actually querying and publishing data from
+        the controller STM board
+        '''
         #Keep trying to initiate contact with board, once established proceed
         try:
-            if self.serialport is None: self.serialport = serial.Serial("/dev/ttyACM0", 115200, timeout=0.5)
+            if self.serial_port is None:
+                self.serial_port = serial.Serial("/dev/ttyACM0", 115200, timeout=0.5)
         except:
             print("Unable to connect to STM board!")
             #Set message to custom datatype
@@ -42,46 +54,50 @@ class ControllerOutput(Node):
             msg.sub_down = 1
             msg.screenshot = 0
             self.com_pub.publish(msg)
-            
-        if self.serialport is not None:        
-            #Read serial data and save to variables
-            self.serialLine = self.serialport.readline()
-            self.serialLine = self.serialLine.decode('ascii')
-            localDat = self.serialLine.strip().strip('\x00')
-            localDat = localDat.split(',')
-            #print(localDat)
 
-            try: 
-                if len(localDat) == 7:
+        if self.serial_port is not None:
+            #Read serial data and save to variables
+            self.serial_line = self.serial_port.readline()
+            self.serial_line = self.serial_line.decode('ascii')
+            local_dat = self.serial_line.strip().strip('\x00')
+            local_dat = local_dat.split(',')
+            #print(local_dat)
+
+            try:
+                if len(local_dat) == 7:
                     #Set message to custom datatype
                     msg = ComInfo()
-                    
+
                     #Normalize & put data in each field of datatype
-                    msg.left_toggle_ud = normalization(int(localDat[0]))
-                    msg.left_toggle_lr = normalization(int(localDat[1]))
-                    msg.right_toggle_ud = (255 -  normalization(int(localDat[2])))
-                    msg.right_toggle_lr =(255 -  normalization(int(localDat[3])))
-                    msg.sub_up = int(localDat[4])
-                    msg.sub_down = int(localDat[5])
-                    msg.screenshot = int(localDat[6])
-                    
+                    msg.left_toggle_ud = normalization(int(local_dat[0]))
+                    msg.left_toggle_lr = normalization(int(local_dat[1]))
+                    msg.right_toggle_ud = (255 -  normalization(int(local_dat[2])))
+                    msg.right_toggle_lr =(255 -  normalization(int(local_dat[3])))
+                    msg.sub_up = int(local_dat[4])
+                    msg.sub_down = int(local_dat[5])
+                    msg.screenshot = int(local_dat[6])
+
                     self.com_pub.publish(msg)
-                    self.get_logger().info('Publishing Left Toggle UD: "%d' % msg.left_toggle_ud) 
-                    self.get_logger().info('Publishing Left Toggle LR: "%d' % msg.left_toggle_lr) 
-                    self.get_logger().info('Publishing Right Toggle UD: "%d' % msg.right_toggle_ud) 
-                    self.get_logger().info('Publishing Right Toggle LR: "%d' % msg.right_toggle_lr) 
-                    self.get_logger().info('Publishing SubUp: "%d' % msg.sub_up)
-                    self.get_logger().info('Publishing SubDown: "%d' % msg.sub_down)  
-                    self.get_logger().info('Publishing Screenshot: "%d' % msg.screenshot) 
-             
+                    self.get_logger().info(f"Publishing Left Toggle UD: {msg.left_toggle_ud}")
+                    self.get_logger().info(f"Publishing Left Toggle LR: {msg.left_toggle_lr}")
+                    self.get_logger().info(f"Publishing Right Toggle UD: {msg.right_toggle_ud}")
+                    self.get_logger().info(f"Publishing Right Toggle LR: {msg.right_toggle_lr}")
+                    self.get_logger().info(f"Publishing SubUp: {msg.sub_up}")
+                    self.get_logger().info(f"Publishing SubDown: {msg.sub_down}")
+                    self.get_logger().info(f"Publishing Screenshot: {msg.screenshot}")
+
             #Sometimes the first data line is bad - no need to crash the entire program
-            #because of it       
+            #because of it
             except ValueError:
                 pass
-            except UnicodeDecodeError:  
+            except UnicodeDecodeError:
                 pass
 
 def normalization(data):
+    '''
+    This normalization method helps keep the toggle drift to a minimum. When "at rest",
+    the values drift slightly. This prevents the sub from acting erratically
+    '''
     ##normal = abs(int((((4.64 * (10 ** -3) * (data ** 2)) - (.182 * data)))))
     #return normal
     if data > 100 and data < 150:
@@ -89,16 +105,21 @@ def normalization(data):
         return data
     else:
         return data
-    
-    #for i in range(len(localDat)):
-    #    normalized_data.append((((4.64 * (10**-3)) * (int(str(localDat[int(i)])) ** 2)) - (.182 * int(str(localDat[int(i)])))))
+
+    #for i in range(len(local_dat)):
+    #    normalized_data.append((((4.64 * (10**-3)) *
+    #    (int(str(local_dat[int(i)])) ** 2)) - (.182 * int(str(local_dat[int(i)])))))
     #return normalized_data
 
 def main(args=None):
+    '''
+    The method spins up the ROS node so that the controller publisher actually
+    runs
+    '''
     rclpy.init(args=args)
     con_obj = ControllerOutput()
     rclpy.spin(con_obj)
     rclpy.shutdown()
-    
+
 if __name__ == '__main__':
-    main()   
+    main()
