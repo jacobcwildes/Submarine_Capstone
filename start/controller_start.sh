56e6293e32de5cb379ee4a7803bc450651b78326
jacobcwildes <wildes126@gmail.com>
2023-12-12 00:30:15 -0500

    Update comments for network side of things

--- start/controller_start.sh
+++ start/controller_start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-#export DISPLAY=:0
+#This shell script is run as a part of the "after_netplan_controller_launch_service". 
+#This actually sources ROS2 workspace and properly runs the launchfile for the ROS nodes
+#Written by Jacob Wildes
+
 source /home/controller/Submarine_Capstone/ros2_ws/install/setup.bash
 ros2 launch controller_gui con_launch.py
 
