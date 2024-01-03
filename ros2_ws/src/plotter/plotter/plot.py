089901b57ed78fd667518b8ba0ef2d46eeb802bb
jacobcwildes <wildes126@gmail.com>
2024-01-03 10:40:59 -0500

    Changed method for movement, made array to store movements

--- ros2_ws/src/plotter/plotter/plot.py
+++ ros2_ws/src/plotter/plotter/plot.py
@@ -12,5 +12,33 @@ class plotter(Node):
         super().__init__('plotter')
         self.path_sub = self.create_subscription(ComInfo, '/random_data',
                                                  self.path_callback, 10)
+        self.counter = 0
+        #Create a NumPy array that contains 1000 indices of length 3 tuples
+        self.array = np.zeros((1000, 3), dtype=int)
+
+    def path_callback(self, path_sub):
+        #print(self.array)
+        #print(self.array.shape)
         
+        #Want 1000 indices
+        if self.counter != 1000:
+            print("Filling array: %d", self.counter)
+            self.array[self.counter] = [path_sub.left_toggle_ud, path_sub.left_toggle_lr, path_sub.sub_up]
+            self.counter += 1
+        else:
+            print(self.array)
         
+
+
+def main(args=None):
+    '''
+    Spin up the ROS node to subscribe
+    '''
+    rclpy.init(args=args)
+    plot = plotter()
+    rclpy.spin(plot)
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
--- ros2_ws/src/random_explore/random_explore/explore.py
+++ ros2_ws/src/random_explore/random_explore/explore.py
@@ -11,16 +11,14 @@ class random_explore(Node):
     def __init__(self):
         super().__init__('explore')
         self.publisher = self.create_publisher(ComInfo, '/random_data', 10)
-        timer_period = .5
+        timer_period = .0025
         self.timer = self.create_timer(timer_period, self.timer_callback)
         #Don't want to have tons of random numbers all the time, this lets me 
         #control how often random generation occurs
         self.i = 0
         self.forward = 0
-        self.left_right = 0
+        self.left = 0
         self.up = 0
-        self.down = 0
-        self.tiebreaker = None
 
     def timer_callback(self):
         '''
@@ -35,39 +33,21 @@ class random_explore(Node):
         sub_down = descend
         '''
         if self.i%1 == 0:
-            #128 is the "0" of the controller. Anything less is a reverse
-            #For now I want to only go forward. I want to have a maximum of 255,
-            #and be able to reach any integer between those numbers
-            #self.forward = random.randrange(128, 255, 28)
+            #0 is stop, 1 is go forward
             self.forward = random.randint(0, 1)
 
         if self.i%1 == 0:
-             #128 is the "0" of the controller. Anything less is a reverse
-            #For now I want to only go forward. I want to have a maximum of 255,
-            #and be able to reach any integer between those numbers
-            #self.left_right = random.randrange(0, 255, 12)
-            self.left_right = random.randint(0, 2)
+             #-1 turns left, 0 is forward, 1 turns right
+            self.left = random.randint(-1, 1)
         
         if self.i%1 == 0:
-            #up and down are binaries, so only want to be a zero or 1
-            self.up = random.randint(0, 1)
-            self.down = random.randint(0, 1)
-
-            if self.up == self.down:
-                self.tiebreaker = random.randint(0, 1)
-
-                if self.tiebreaker == 0:
-                    self.up = 1
-                    self.down = 0
-                else:
-                    self.up = 0
-                    self.down = 1
+            #-1 goes down, 0 stays level, 1 goes up
+            self.up = random.randint(-1, 1)
 
         msg = ComInfo()
         msg.left_toggle_ud = self.forward
-        msg.left_toggle_lr = self.left_right
+        msg.left_toggle_lr = self.left
         msg.sub_up = self.up
-        msg.sub_down = self.down
         self.publisher.publish(msg)
         self.get_logger().info("Publishing data")
         self.i += 1
